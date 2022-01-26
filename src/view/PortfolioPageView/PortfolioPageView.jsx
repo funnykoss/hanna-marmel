f95dd76fbf45bfe4fd2732d880c420756f77d9c7
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-26 22:43:12 +0200

    portfolio

--- src/view/PortfolioPageView/PortfolioPageView.jsx
+++ src/view/PortfolioPageView/PortfolioPageView.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import s from './PortfolioPageView.module.css'
 import PortfolioGallery from '../../components/PortfolioGallery/'
 import list from '../../JSON/portfolio.json'
 import Container from "../../components/Container/Container";
+import { MdOutlineDoubleArrow } from 'react-icons/md';
+
 
 
 const PortfolioPageView = () => {
@@ -10,8 +13,12 @@ const PortfolioPageView = () => {
         
         <section className={s.portfolioSection}>
             <Container>
+                <h1 className={s.sectionTitle}> Unusual and custom characters</h1>
                 <PortfolioGallery list={list} />
-                </Container>
+                <Link to="/portrets" className={s.link}>
+                    <MdOutlineDoubleArrow className={ s.icon}/>
+                </Link>
+             </Container>
             </section>
        
     )
