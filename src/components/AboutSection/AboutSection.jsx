ae64884d31aa05b38f44af857f6bf5082af679d2
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-01-26 13:16:29 +0200

    css social link

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -2,42 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import photo from '../../images/about/hanna-MarMel.jpeg'
 import { BsArrowRightShort } from 'react-icons/bs';
-import { AiOutlineInstagram } from 'react-icons/ai';
-import { BsFacebook } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
+import { RiFacebookCircleFill } from 'react-icons/ri';
 import s from './AboutSection.module.css'
 import Container from '../Container/Container'
 
 const AboutSection = () => {
 
-    const instagram = "https://www.instagram.com/hannamarmel/"
     return (
     
         <section className={s.sectionAbout}>
             <Container>
                 <div className={s.wrapper}>
-                    
-                 <h1 className={s.sectionTitle}>Hanna Martysheva</h1>
+                    <h1 className={s.sectionTitle}>Hanna Martysheva</h1>
                     <h2 className={s.description}>photographer</h2>
                     <ul className={s.socialLinks}>
-                        <li> 
+                        <li className={s.socialItem}> 
                             <a href="https://www.instagram.com/hannamarmel/" target="_blank" rel="noreferrer"> 
-                             <AiOutlineInstagram className={s.socialIcon}/> 
+                             <AiFillInstagram className={s.socialIcon}/> 
                             </a>
                         </li>
-                        <li>
+                        <li className={s.socialItem}>
                             <a href="https://www.facebook.com/MAnyapeace" target="_blank" rel="noreferrer">
-                            <BsFacebook className={s.socialIcon}/>     
+                            <RiFacebookCircleFill className={s.socialIcon}/>     
                             </a> 
                         </li>
-                    </ul>
-                       
+                    </ul>   
                 </div>
-                <div className={s.linkWrapper}>
+                <Link to='/portfolio'className={s.aboutLink}>
                             <BsArrowRightShort className={ s.icon}/>
-                            <Link to='/portfolio' className={s.aboutLink}>
+                          
                                 SHOW PORTFOLIO        
-                            </Link>
-                 </div>
+                           
+                 </Link>
                  <div className={s.imgWrapper}>
                     <img src={photo} alt="Анна Мартышева фото" className={s.img}/>
                 </div> 
