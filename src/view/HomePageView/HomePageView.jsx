d366e65427b6e1f4a994e904e8774439da763d3e
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-06-29 13:41:05 +0200

    css

--- src/view/HomePageView/HomePageView.jsx
+++ src/view/HomePageView/HomePageView.jsx
@@ -2,11 +2,34 @@ import React from "react";
 import PortfolioGallery from '../../components/PortfolioGallery/PortfolioGallery'
 import Container from "../../components/Container";
 import list from '../../JSON/start.json'
+// import { BsArrowRightShort } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
+import { RiFacebookCircleFill } from 'react-icons/ri';
+import s from './HomePageView.module.css'
+
 
 
 const HomePageView = () => {
     return (
         <Container>
+            <div className={s.sectionInfo}>
+                    <div className={s.photograferName}>
+                        <h1 className={s.sectionTitle}>Hanna Martysheva</h1>
+                        <h2 className={s.description}>photographer</h2>
+                    </div>
+                    <ul className={s.socialLinks}>
+                        <li className={s.socialItem}> 
+                            <a href="https://www.instagram.com/hannamarmel/" target="_blank" rel="noreferrer"> 
+                             <AiFillInstagram className={s.socialIcon}/> 
+                            </a>
+                        </li>
+                        <li className={s.socialItem}>
+                            <a href="https://www.facebook.com/MAnyapeace" target="_blank" rel="noreferrer">
+                            <RiFacebookCircleFill className={s.socialIcon}/>     
+                            </a> 
+                        </li>
+                    </ul>
+            </div> 
             <PortfolioGallery list={list}/>
         </Container>
     )
