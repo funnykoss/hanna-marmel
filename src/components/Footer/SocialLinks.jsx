26c948517127f8db3dce42e24b34fd0107e95d45
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-07-17 15:19:40 +0200

    add component Contakts

---
+++ src/components/Footer/SocialLinks.jsx
@@ -0,0 +1,13 @@
+const SocialLinks = () => {
+  return (
+    <ul>
+      <li>
+        <a href="mailto:manyapeace48gmail.com">e-mail</a>
+      </li>
+      <li>
+        <a href="tel:+380666549757">+380 66 654 9757</a>
+      </li>
+    </ul>
+  );
+};
+export default SocialLinks;
