4c8bfec0d8a56e11b0ea4981184c96d4dc303846
funnykoss <75586141+funnykoss@users.noreply.github.com>
2022-07-17 15:07:38 +0200

    footer css add rewiews

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 import s from './Footer.module.css'
-import SendMail from '../SendMail/SendMail'
+import Reviews from '../Reviews/Reviews'
 import Container from "../Container";
 import Contacts from "./Contacts";
 
 const Footer = () => {
     return (
         <Container>
+            <div className={s.footer}>
                 <Contacts/>
-            {/* <SendMail /> */}
+                <Reviews />
+            </div>
+                
         
         </Container>
        
