4bf3b7b87151f577f2a26c2d5cb0c50343d512ab
funnykoss <75586141+funnykoss@users.noreply.github.com>
2023-02-22 16:26:20 +0100

    сии

--- src/components/Biografy/Biography.jsx
+++ src/components/Biografy/Biography.jsx
@@ -1,17 +1,14 @@
-import Container from '../Container';
 import s from './Biography.module.css';
 
 const Biography = () => {
   return (
     <div className={s.section}>
-      <Container>
-        <h2 className={s.title}> Hanna Martysheva</h2>
-        <p className={s.description}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sapiente molestias
-          mollitia, aspernatur nostrum consequatur itaque harum adipisci voluptas iure? Aut quam
-          voluptatum libero atque tempore deserunt fugiat cupiditate ipsa!
-        </p>
-      </Container>
+      <h2 className={s.title}> Hanna Martysheva</h2>
+      <p className={s.description}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sapiente molestias
+        mollitia, aspernatur nostrum consequatur itaque harum adipisci voluptas iure? Aut quam
+        voluptatum libero atque tempore deserunt fugiat cupiditate ipsa!
+      </p>
     </div>
   );
 };
--- src/view/DronePageView/DronePageView.jsx
+++ src/view/DronePageView/DronePageView.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import s from './DronePageView.module.css';
 import PortfolioGallery from '../../components/PortfolioGallery';
