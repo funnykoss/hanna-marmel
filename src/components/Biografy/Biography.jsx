f5b93e2e4c5e347d858b38397b60e23c6215f04f
funnykoss <75586141+funnykoss@users.noreply.github.com>
2023-11-15 20:11:13 +0100

    biografy

--- src/components/Biografy/Biography.jsx
+++ src/components/Biografy/Biography.jsx
@@ -5,9 +5,11 @@ const Biography = () => {
     <div className={s.section}>
       <h2 className={s.title}> Hanna Martysheva</h2>
       <p className={s.description}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit sapiente molestias
-        mollitia, aspernatur nostrum consequatur itaque harum adipisci voluptas iure? Aut quam
-        voluptatum libero atque tempore deserunt fugiat cupiditate ipsa!
+        Hello, my name is Anna, I am a photographer with more than 7 years of experience. I am
+        constantly looking for ideas for development and implementation, responsible, creative and
+        sociable. I am familiar with digital and analog photographic equipment. I am proficient in
+        Photoshop and Lightroom. Confident PC user. The level of German is beginner, but I am in the
+        process of learning and am very motivated. I will be glad to become part of your team!
       </p>
     </div>
   );
--- src/view/ContactsPageView/ContactsPageView.jsx
+++ src/view/ContactsPageView/ContactsPageView.jsx
@@ -1,14 +1,14 @@
-import Contacts from "../../components/Contacts/Contacts";
-import Container from "../../components/Container";
-import Reviews from "../../components/Reviews/Reviews";
-import s from "./ContactsPageView.module.css";
+import Contacts from '../../components/Contacts/Contacts';
+import Container from '../../components/Container';
+import Reviews from '../../components/Reviews/Reviews';
+import s from './ContactsPageView.module.css';
 
 const ContactsPageView = () => {
   return (
     <Container>
       <div className={s.contactsSection}>
         <Contacts />
-        <Reviews />
+        {/* <Reviews /> */}
       </div>
     </Container>
   );
